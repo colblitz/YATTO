90079f6067bc8151080b92d71f9e946fc83f4869
colblitz <z.joseph.lee.z@gmail.com>
2016-06-04 02:12:55 -0400

    cleanup

--- public/javascripts/controllers/OtherCalcController.js
+++ public/javascripts/controllers/OtherCalcController.js
@@ -86,20 +86,6 @@ yattoApp.controller('OtherCalcController',
           class: $scope.customizationClass[c.ctype],
         });
       });
-      // $scope.oc_customizations = [[], [], [], [], [], []];
-      // $scope.oc_customization_totals = [0, 0, 0, 0, 0, 0];
-      // customization_info.forEach(function(c, i) {
-      //   var ce = c.value / c.cost;
-      //   $scope.oc_customizations[cMapping[c.label[0]]].push({
-      //     name: c.name,
-      //     value: c.value,
-      //     cost: c.cost,
-      //     ctype: c.ctype,
-      //     type: c.type,
-      //     coste: isNaN(ce) ? 0 : ce,
-      //     owned: c.ctype != CTYPE_D
-      //   });
-      // });
 
       $scope.critss = 0;
       $scope.zerker = 0;
@@ -155,15 +141,6 @@ yattoApp.controller('OtherCalcController',
       return transformScopeArray($scope.heroes.map(function (h) {
         return {index: h.id - 1, value: h.level[$rootScope.world]}; }));
     };
-
-    // var getGameState = function(c) {
-    //   return new GameState(
-    //     transformScopeArray($scope.artifacts),
-    //     getWeapons(),
-    //     getLevels(),
-    //     c,
-    //     { cs: $scope.critss, br: $scope.zerker });
-    // };
 
     var getGameState = function(c) {
       return new GameState({
@@ -227,9 +204,7 @@ yattoApp.controller('OtherCalcController',
       // index-0s are now a "min-heap"
       var tempTotals = $scope.customizationTotals.slice();
       while (tempC.length > 1) {
-        // console.log("temp totals: ", tempTotals);
         var baseValue = getDmgE(tempTotals);
-        // console.log("baseValue: ", baseValue);
         var max = 0;
         var maxi = -1;
         // compare options
@@ -238,10 +213,7 @@ yattoApp.controller('OtherCalcController',
           var newTotals = tempTotals.slice();
           newTotals[c.type] += c.value;
 
-          // console.log("new totals: ", newTotals);
-
           var newValue = getDmgE(newTotals);
-          // console.log("newValue: ", newValue);
 
           var goldRatio = newValue.gold / baseValue.gold;
           var tdmgRatio = newValue.tdmg / baseValue.tdmg;
@@ -251,9 +223,6 @@ yattoApp.controller('OtherCalcController',
           var tdmgEquivalent = baseValue.tdmg * tdmgEquivalentRatio;
 
           var efficiency = (tdmgEquivalent - baseValue.tdmg) / c.cost;
-          // console.log("----------------");
-          // console.log(efficiency);
-          // console.log(max);
           if (efficiency > max) {
             max = efficiency;
             maxi = t;
@@ -270,89 +239,6 @@ yattoApp.controller('OtherCalcController',
         tempTotals[bestC.type] += bestC.value;
         $scope.customizationsToBuy.push(bestC);
       }
-
-      // $scope.diamonds = 0;
-      // $scope.unowned_customizations = [];
-      // $scope.oc_customization_totals = [0, 0, 0, 0, 0, 0];
-
-      // var tc = [[], [], [], [], [], []];
-      // for (var type in $scope.oc_customizations) {
-      //   $scope.oc_customizations[type].forEach(function(c, i) {
-      //     if (!c.owned && c.ctype == CTYPE_D) {
-      //       tc[type].push(c);
-      //       $scope.diamonds += c.cost;
-      //     }
-      //     if (c.owned) {
-      //       $scope.oc_customization_totals[c.type] += c.value / 100;
-      //     }
-      //   });
-      // }
-
-      // tc = tc.filter(function(n){ return n.length != 0 });
-
-      // // sort unowned by efficiency
-      // for (var type in tc) {
-      //   tc[type] = tc[type].sort(function(c1, c2) {
-      //     return c2.coste - c1.coste;
-      //   });
-      // }
-
-      // var temp_totals = $scope.oc_customization_totals.slice(0);
-      // while (tc.length > 1) {
-      //   var base = getDmgE(temp_totals);
-      //   var max = 0;
-      //   var maxi = -1;
-      //   for (t in tc) {
-      //     var c = tc[t][0];
-      //     var new_totals = temp_totals.slice(0);
-      //     new_totals[c.type] += c.value / 100;
-      //     var new_value = getDmgE(new_totals);
-
-      //     var gold_ratio = new_value[0] / base[0];
-      //     var tdmg_ratio = new_value[1] / base[1];
-      //     var gold_dmg_equivalent = Math.pow(1.044685, Math.log(gold_ratio) / Math.log(1.075));
-      //     var eq_tdmg = (gold_dmg_equivalent - 1) * base[1] + new_value[1];
-      //     var eff = (eq_tdmg - base[1]) / c.cost;
-      //     if (eff > max) {
-      //       max = eff;
-      //       maxi = t;
-      //     }
-      //   }
-      //   if (maxi == -1) {
-      //     console.log("alskdjfljasldjjaaaaaaA");
-      //     console.log(tc);
-      //   }
-      //   var bestc = tc[maxi].shift();
-      //   if (tc[maxi].length == 0) {
-      //     tc.splice(maxi, 1);
-      //   }
-      //   temp_totals[bestc.type] += bestc.value / 100;
-      //   $scope.unowned_customizations.push(bestc);
-      // }
-
-      // var base = getDmgE($scope.oc_customization_totals);
-      // console.log("base: " + base);
-      // for (i in $scope.unowned_customizations) {
-      //  console.log("--------------------------------");
-      //  var c = $scope.unowned_customizations[i];
-      //  var new_totals = $scope.oc_customization_totals.slice(0);
-      //  new_totals[c.type] += c.value / 100;
-      //  console.log(new_totals);
-      //  var new_value = getDmgE(new_totals);
-
-      //  var gold_ratio = new_value[0] / base[0];
-      //  var tdmg_ratio = new_value[1] / base[1];
-      //  var gold_dmg_equivalent = Math.pow(1.044685, Math.log(gold_ratio) / Math.log(1.075));
-      //  var eq_tdmg = (gold_dmg_equivalent - 1) * base[1] + new_value[1];
-      //  c.efficiency = (eq_tdmg - base[1]) / c.cost;
-
-      //  console.log(c.name + ":");
-      //  console.log("   " + c.cost);
-      //  console.log("   " + c.efficiency);
-      // }
-      // $scope.unowned_customizations = $scope.unowned_customizations.sort(function(c1, c2) {
-      //  c1.efficiency - c2.efficiency;
-      // });
     };
 
     $scope.setAll = function() {
@@ -361,9 +247,6 @@ yattoApp.controller('OtherCalcController',
         $scope.heroes.forEach(function(h, i) {
           h.level[$rootScope.world] = l;
         });
-        // $scope.oc_heroes.forEach(function(h, i) {
-        //   h.level = l;
-        // });
       }
       $scope.updateLevels();
     };
@@ -442,105 +325,8 @@ yattoApp.controller('OtherCalcController',
 
       $scope.targetGold = parseFloat(requiredGold.toPrecision(4)).toExponential()
       var g = getGameState($scope.customizationTotals);
-      console.log(g.getGoldMultiplier());
-      console.log(baseStageGold($scope.targetStage));
       var goldPerMob = g.getGoldMultiplier() * baseStageGold($scope.targetStage);
       $scope.targetMonsters = parseFloat(Math.round(requiredGold / goldPerMob).toPrecision(4)).toExponential();
-
-      // var gold_left = gold;
-      // var gold100 = gold / 100;
-      // var levels_new = levels.slice();
-
-      // // console.log(levels_new);
-
-      // hero_info.forEach(function(h, i) {
-      //   var l = levels_new[i];
-      //   var cost = h.cost_to_level(l, l + 100);
-      //   while (cost < gold100) {
-      //     l += 100;
-      //     gold_left -= cost;
-      //     cost = h.cost_to_level(l, l + 100);
-      //   }
-      //   levels_new[i] = l;
-      // });
-
-      // // console.log(gold_left);
-      // // console.log(levels_new);
-
-      // var gold10 = gold_left / 100;
-      // hero_info.forEach(function(h, i) {
-      //   var l = levels_new[i];
-      //   var cost = h.cost_to_level(l, l + 10);
-      //   while (cost < gold10) {
-      //     l += 10;
-      //     gold_left -= cost;
-      //     cost = h.cost_to_level(l, l + 10);
-      //   }
-      //   levels_new[i] = l;
-      // });
-
-      // // console.log(gold_left);
-      // // console.log(levels_new);
-
-      // var lheap = binaryHeap(function(a, b) {
-      //   return a[0] < b[0];
-      // });
-
-      // hero_info.forEach(function(h, i) {
-      //   var l = levels_new[i];
-      //   var cost = h.get_upgrade_cost(l);
-      //   lheap.push([cost, i]);
-      // });
-
-      // var steps = 0;
-      // while (gold_left > 0) {
-      //   steps += 1;
-      //   var t = lheap.pop();
-      //   var cost = t[0];
-      //   var index = t[1];
-      //   if (gold_left > cost) {
-      //     gold_left -= cost;
-      //     levels_new[index] += 1;
-      //     var new_cost = hero_info[index].get_upgrade_cost(levels_new[index]);
-      //     lheap.push([new_cost, index]);
-      //   } else {
-      //     break;
-      //   }
-      // }
-
-      // $scope.oc_heroes.forEach(function(h, i) {
-      //   h.best = levels_new[i];
-      // });
-
-      // $scope.total_levels = levels_new.reduce(function(a, b) { return a + b; });
-
-      // var current = $scope.total_levels;
-      // var required_gold = 0;
-      // while (current < $scope.target_levels) {
-      //   var t = lheap.pop();
-      //   var cost = t[0];
-      //   var index = t[1];
-      //   required_gold += cost;
-      //   current += 1;
-      //   levels_new[index] += 1;
-      //   var new_cost = hero_info[index].get_upgrade_cost(levels_new[index]);
-      //   lheap.push([new_cost, index]);
-      // }
-      // console.log(required_gold);
-      // $scope.target_gold = parseFloat(required_gold.toPrecision(4)).toExponential();
-
-      // base_stage_gold($scope.target_stage)
-
-      // var g = getGameState($scope.oc_customization_totals);
-      // g.get_all_skills();
-
-      // var gold_per_mob = g.mob_multiplier() * base_stage_gold($scope.target_stage);
-      // $scope.target_monsters = parseFloat(Math.round(required_gold / gold_per_mob).toPrecision(4)).toExponential();
-
-      // console.log(gold_left);
-      // console.log(levels_new);
-      // console.log(levels_new.reduce(function(a, b) { return a + b; }));
-      // console.log(steps);
     };
 
     $scope.stateChanged = function() {
--- public/javascripts/controllers/ReferenceController.js
+++ public/javascripts/controllers/ReferenceController.js
@@ -36,18 +36,6 @@ yattoApp.controller('ReferenceController',
       [BonusTypes.SKILL_DRN_GOLD, "Gold Skill duration"],
       [BonusTypes.SKILL_DRN_HERO, "Hero Skill duration"],
       [BonusTypes.SKILL_DRN_TDMG, "TDmg Skill duration"],
-
-      // [BonusTypes.SKILL_CDR_AUTO, "Shadow Clone/Summon Golem cooldown"],
-      // [BonusTypes.SKILL_CDR_CRIT, "Critical Strike/Poisonous Touch cooldown"],
-      // [BonusTypes.SKILL_CDR_GOLD, "Hand of Midas/Chesterson Streak cooldown"],
-      // [BonusTypes.SKILL_CDR_HERO, "War Cry/Inspiring Presence cooldown"],
-      // [BonusTypes.SKILL_CDR_OHKO, "Heavenly Strike/Heroic Burst cooldown"],
-      // [BonusTypes.SKILL_CDR_TDMG, "Berserker Rage/Arcane Channeling cooldown"],
-      // [BonusTypes.SKILL_DRN_AUTO, "Shadow Clone/Summon Golem duration"],
-      // [BonusTypes.SKILL_DRN_CRIT, "Critical Strike/Poisonous Touch duration"],
-      // [BonusTypes.SKILL_DRN_GOLD, "Hand of Midas/Chesterson Streak duration"],
-      // [BonusTypes.SKILL_DRN_HERO, "War Cry/Inspiring Presence duration"],
-      // [BonusTypes.SKILL_DRN_TDMG, "Berserker Rage/Arcane Channeling duration"],
       [BonusTypes.TAP_DAMAGE_ARTIFACTS, "Tap Damage"],
       [BonusTypes.UPGRADE_COST, "Upgrade Cost"],
     ];
@@ -84,16 +72,6 @@ yattoApp.controller('ReferenceController',
                    BonusTypes.NUM_MOBS,
                    BonusTypes.RELICS],
     };
-
-//     .skill-dps, .bonus-o { background-color: #CFE2F3; }
-// .skill-ad, .bonus-ad { background-color: #D9D2E9; }
-// .skill-cd, .bonus-cd { background-color: #94DFD9; }
-// .skill-td, .bonus-td { background-color: #F4CCCC; }
-// .skill-pd, .bonus-pd { background-color: #EBACC8; }
-// .skill-cg, .bonus-cg { background-color: #EBD699; }
-// .skill-gd, .bonus-gd { background-color: #FFF2CC; }
-// .skill-bd, .bonus-bd { background-color: #F3CDB1; }
-// .skill-cc, .bonus-cc { background-color: #D9EAD3; }
 
     var bonusTypeClasses = {};
     for (var bClass in bTypeClasses) {
@@ -170,74 +148,6 @@ yattoApp.controller('ReferenceController',
       }
 
       $scope.sumCumulative = 0;
-
-      // $scope.r_artifacts = [];
-      // $scope.sum_cumulative = 0;
-
-      // for (var i in artifact_info) {
-      //   var a = artifact_info[i];
-      //   var artifact = {
-      //     name: a.name,
-      //     index: i,
-      //     ad0: a.ad0,
-      //     adpl: a.adpl,
-      //     cap: isFinite(a.levelcap) ? a.levelcap : null,
-      //     x: a.x.toFixed(1),
-      //     y: a.y.toFixed(1),
-      //     current: 0,
-      //     desired: 0,
-      //     cost: 0,
-      //     cumulative: 0,
-      //     magnitude: "",
-      //     text: "",
-      //     costf: a.cost,
-      //     effect: a.effect,
-      //     description: a.description
-      //     // test: function(l) { return Math.round(a.cost(l)); } // TODO: wtf doesn't this work
-      //   };
-      //   $scope.r_artifacts.push(artifact);
-      // }
-
-      // $scope.r_heroes = [];
-      // var skill_types = [
-      //   "Hero DPS",
-      //   "All damage",
-      //   "Crit damage",
-      //   "Tap damage",
-      //   "Percent DPS",
-      //   "Chest Gold",
-      //   "Gold Dropped",
-      //   "Boss Damage",
-      //   "Crit Chance"
-      // ];
-
-      // var typeclasses = [
-      //   "skill-dps",
-      //   "skill-ad",
-      //   "skill-cd",
-      //   "skill-td",
-      //   "skill-pd",
-      //   "skill-cg",
-      //   "skill-gd",
-      //   "skill-bd",
-      //   "skill-cc"
-      // ];
-
-      // for (var h in hero_info) {
-      //   var h = hero_info[h];
-      //   var hero = {
-      //     name: h.name,
-      //     cost: h.base_cost.toPrecision(4),
-      //     skills: h.skills.map(function(s) {
-      //       return {
-      //         magnitude: s[0] * 100 > 1 ? Math.round(s[0] * 100) : s[0] * 100,
-      //         type: skill_types[s[1]],
-      //         typeclass: typeclasses[s[1]]
-      //       };
-      //     })
-      //   };
-      //   $scope.r_heroes.push(hero);
-      // }
     };
 
     var updateMagnitude = function(a) {
@@ -255,12 +165,6 @@ yattoApp.controller('ReferenceController',
         });
       }
       console.log(a.magnitudes);
-      // a.magnitudes = mapMap(a.effects, function(etype) {
-      //   return {
-      //     value: a.effects[etype] * l,
-      //     text: bonusTypeText[etype],
-      //   };
-      // });
     };
 
     var updateText = function(a) {
@@ -290,46 +194,6 @@ yattoApp.controller('ReferenceController',
       $scope.sumCumulative = $scope.refArtifacts[$rootScope.world].map(function(a) { return a.cumulative; }).reduce(function(a, b) { return a + b; }, 0);
     };
 
-    // var getMagnitude = function(a) {
-    //   var l = Math.max(0, Math.min(a.desired, a.cap == null ? Infinity : a.cap));
-    //   if (l == 0) {
-    //     return "";
-    //   }
-    //   var s = (l * a.effect).toString() + "% ";
-    //   if (a.effect > 0) {
-    //     s = "+" + s;
-    //   }
-    //   return s;
-    // };
-
-    // var getText = function(a) {
-    //   var s = a.description;
-    //   if (s.indexOf("duration") > -1) {
-    //     s = s + "(" + Math.round(30 * ((a.desired * 10) / 100 + 1)).toString() + " s without cooldown)";
-    //   }
-    //   return s;
-    // };
-
-    // $scope.calcArtifacts = function(i) {
-    //   var a = $scope.r_artifacts[i];
-    //   if (a.current != 0) {
-    //     a.cost = Math.ceil(a.costf(a.current+1));
-    //     a.cumulative = 0;
-    //     for (var l = a.current; l < a.desired; l++) {
-    //       a.cumulative += Math.ceil(a.costf(l+1));
-    //     }
-    //   } else {
-    //     a.cost = 0;
-    //     a.cumulative = 0;
-    //   }
-    //   a.magnitude = getMagnitude(a);
-    //   a.text = getText(a);
-
-    //   $scope.sum_cumulative = $scope.r_artifacts
-    //     .map(function(a) { return a.cumulative; })
-    //     .reduce(function(a, b) {return a + b; }, 0);
-    // };
-
     $scope.updateFromState = function() {
       var artifactLevels = {};
       for (var w in $rootScope.state.artifacts) {
@@ -348,77 +212,6 @@ yattoApp.controller('ReferenceController',
           $scope.calcArtifact(artifact);
         }
       }
-
-      // var a = artifacts[i];
-      //   $scope.r_artifacts[a.index].current = a.value;
-      //   $scope.r_artifacts[a.index].desired = a.value;
-      // }
-      // for (var i in $scope.r_artifacts) {
-      //   $scope.calcArtifacts(i);
-
-      // for (var i in artifactInfo) {
-      //   var a = artifactInfo[i];
-      //   $scope.refArtifacts[a.world].push({
-      //     name: a.name,
-      //     id: a.id,
-      //     index: i,
-      //     ad0: a.ad0,
-      //     adpl: a.adpl,
-      //     cap: a.levelcap,
-      //     x: a.x.toFixed(1),
-      //     y: a.y.toFixed(1),
-      //     current: 0,
-      //     desired: 0,
-      //     cost: 0,
-      //     cumulative: 0,
-      //     effects: a.effects,
-      //     magnitudes: a.effects.map(function(e) { return { value: 0, text: ""}; }),
-      //     costFunction: a.cost,
-      //     flavortext: a.flavor
-      //   });
-      // }
-
-
-
-      // $scope.artifacts = {
-      //     1: $rootScope.state.artifacts[1].map(function(p) {
-      //       return {
-      //         name: artifactMapping[p[0]].name,
-      //         id: p[0],
-      //         level: p[1],
-      //       };}),
-      //     2: $rootScope.state.artifacts[2].map(function(p) {
-      //       return {
-      //         name: artifactMapping[p[0]].name,
-      //         id: p[0],
-      //         level: p[1],
-      //       };}),
-      //   };
-
-
-
-      // var t = $rootScope.state.split("|");
-
-      // var artifacts = [];
-      // t[1].split(",").forEach(function(a, i, array) {
-      //   var v = a.split(".");
-      //   var aindex = parseOrZero(v[0], parseInt);
-      //   var avalue = parseOrZero(v[1], parseInt);
-      //   artifacts.push({
-      //     name: artifact_info[aindex].name,
-      //     index: aindex,
-      //     value: avalue
-      //   });
-      // });
-
-      // for (var i in artifacts) {
-      //   var a = artifacts[i];
-      //   $scope.r_artifacts[a.index].current = a.value;
-      //   $scope.r_artifacts[a.index].desired = a.value;
-      // }
-      // for (var i in $scope.r_artifacts) {
-      //   $scope.calcArtifacts(i);
-      // }
     };
 
     $scope.$on('stateUpdate', function() {
